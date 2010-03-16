35ec2234072627afe0574c2eed6a24ef997f2482
Bryan Kearney <bkearney@redhat.com>
2010-03-16 15:23:06 -0400

    move the headpin code over to resteasy json format

--- app/models/candlepin_object.rb
+++ app/models/candlepin_object.rb
@@ -38,24 +38,28 @@ class CandlepinObject < ActiveResource::Base
     
     def instantiate_collection(collection, prefix_options = {})
       Array items = Array.new() 
+      puts (collection.class)
+      puts (collection[1].class)
       if (! collection.nil?)
         # Need to make the result an array of hashmaps in order to 
         # parse it well.
-        inner_collection = Array.new()
+        inner_collection = Array.new()       
+        inner_collection = collection if collection.is_a?(Array)
+        
         # handle the case of {key => {hash of attributes}} which is single object
         # as well as {hash of attributes}
         if (collection.is_a?(Hash))
           if (collection.key?(element_name) && collection.length == 1)
             inner_collection = collection[element_name]
           else
-            inner_collection = collection
+            inner_collection << collection
           end
         end
         # Handle the case of a single string. This may be a bit of a hack :)
         inner_collection = {"value" => collection} if collection.is_a?(String)                   
         # We need this to be an array of hashs {key => [array of hashses]}, 
         # so if it is a single hash we array it.
-        inner_collection = [] << inner_collection if inner_collection.is_a?(Hash)               
+        #inner_collection = [] << inner_collection if inner_collection.is_a?(Hash)   
         inner_collection.each do |record|
           items << instantiate_record(record, prefix_options)
         end
@@ -66,7 +70,16 @@ class CandlepinObject < ActiveResource::Base
   end
   
   def load(attributes)
-    new_attributes = attributes 
+    
+    # Need to handle the case where the attribute is actually a hash
+    # with a single value which is the element name. In that case, the 
+    # real attributes are actually the children
+    if (attributes.size == 1 && attributes[self.class.element_name])
+      new_attributes = attributes[self.class.element_name]
+    else
+      new_attributes = attributes
+    end
+    
     # Handle the case of a single string. This may be a bit of a hack :)
     new_attributes = {"value" => attributes} if attributes.is_a?(String)   
     # Put in the default relationsips
@@ -89,4 +102,8 @@ class CandlepinObject < ActiveResource::Base
   def Id
     self.id
   end
+  
+  def encode(options={})
+    self.class.format.encode({self.class.element_name => attributes}, options)
+  end  
 end
